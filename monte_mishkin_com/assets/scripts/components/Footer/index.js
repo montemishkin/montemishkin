84f78c1932a5f63db587dca81c77f425abd95bbe
Monte Mishkin <montemishkin@gmail.com>
2015-09-02 02:40:14 -0700

    improved sitewide footer

--- monte_mishkin_com/assets/scripts/components/Footer/index.js
+++ monte_mishkin_com/assets/scripts/components/Footer/index.js
@@ -3,6 +3,7 @@ import React from 'react/addons'
 import radium from 'radium'
 /* local imports */
 import styles from './styles'
+import Link from '../Link'
 
 
 /**
@@ -13,7 +14,40 @@ import styles from './styles'
 class Footer extends React.Component {
     render() {
         return (<div style={styles.container}>
-            copyright 2015
+            <span style={styles.left}>
+                <Link
+                    to='home'
+                    style={styles.link}
+                >
+                    Home
+                </Link>
+                <Link
+                    to='about'
+                    style={styles.link}
+                >
+                    About
+                </Link>
+                <Link
+                    to='projects'
+                    style={styles.link}
+                >
+                    Projects
+                </Link>
+                <Link
+                    to='blog'
+                    style={styles.link}
+                >
+                    Blog
+                </Link>
+            </span>
+            <span style={styles.right}>
+                &copy; 2015 <a
+                    href='mailto:montemishkin@gmail.com'
+                    style={styles.link}
+                >
+                    Monte Mishkin
+                </a>
+            </span>
         </div>)
     }
 }
--- monte_mishkin_com/assets/scripts/components/Footer/styles.js
+++ monte_mishkin_com/assets/scripts/components/Footer/styles.js
@@ -9,9 +9,26 @@ import colors from '../../../styles/colors'
 // define style sheet
 let styles = {
     container: {
+        paddingTop: 15,
+        fontSize: 12,
         color: colors.grey.main,
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+    },
+
+    left: {
+        display: 'flex',
+        flexBasis: '30%',
+        justifyContent: 'space-between',
+    },
+
+    right: {
+    },
+
+    link: {
+        color: colors.grey.lightest,
+        textDecoration: 'none',
     },
 }
 
