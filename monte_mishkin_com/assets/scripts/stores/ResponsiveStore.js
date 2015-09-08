3501dccff217a14df003d9c27f7d30c83770bfc6
Monte Mishkin <montemishkin@gmail.com>
2015-09-08 00:21:16 -0700

    added ResponsiveStore

---
+++ monte_mishkin_com/assets/scripts/stores/ResponsiveStore.js
@@ -0,0 +1,10 @@
+/* common alt imports */
+import alt from '../alt-instance'
+import ResponsiveStore from 'alt-responsive'
+
+
+// export alt singleton store
+export default alt.createStore(ResponsiveStore)
+
+
+// end of file
