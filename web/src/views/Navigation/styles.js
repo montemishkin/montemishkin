2ecb825edae42d5fd7e085aa2f6fb35fc406d36a
Monte Mishkin <monte@mishkin.com>
2015-11-28 22:43:12 -0800

    now applying site-wide background-color in both Navigation component and main.css

--- web/src/views/Navigation/styles.js
+++ web/src/views/Navigation/styles.js
@@ -6,7 +6,9 @@ import {mainFontSize} from 'assets/styles/js/numerics'
 export default {
     container: {
         color: colors.text,
+        backgroundColor: colors.textInverse,
         fontSize: mainFontSize,
+
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100%',
