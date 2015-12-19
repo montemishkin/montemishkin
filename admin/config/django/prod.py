5d5023f9a577ccfca55d25a76dcdea9b1bd822aa
Monte Mishkin <monte@mishkin.com>
2015-12-19 00:52:37 -0800

    admin service now runs at admin.monte.mishkin.com in production (localhost:8001 in dev)

--- admin/config/django/prod.py
+++ admin/config/django/prod.py
@@ -10,13 +10,17 @@ from .base import *
 # disable debugging support
 DEBUG = False
 
-# grab secret key from file in production
-with open(os.path.abspath(os.path.join(__file__, os.pardir, 'secret-key'))) as secret_key_file:
-    SECRET_KEY = secret_key_file.read().strip()
+# name of host to use when forming URL's
+# TODO: seems wrong to need this, but currently needed for image URL's...
+HOST_NAME = 'admin.monte.mishkin.com'
 
 # List of allowed hostnames
 # https://docs.djangoproject.com/en/1.8/ref/settings/#std:setting-ALLOWED_HOSTS
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['admin.monte.mishkin.com', 'www.admin.monte.mishkin.com']
+
+# grab secret key from file in production
+with open(os.path.abspath(os.path.join(__file__, os.pardir, 'secret-key'))) as secret_key_file:
+    SECRET_KEY = secret_key_file.read().strip()
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
