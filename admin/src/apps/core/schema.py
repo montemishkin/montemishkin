b291b090b865c549a18c4f9581176625f15bf72c
Monte Mishkin <monte@mishkin.com>
2016-01-17 00:14:20 -0800

    updated graphene version. further fleshed out graphql api.

--- admin/src/apps/core/schema.py
+++ admin/src/apps/core/schema.py
@@ -7,7 +7,7 @@ import graphene
 from graphene.contrib.django import DjangoNode
 # local imports
 from django.conf import settings
-from .models import Post as PostModel, Project as ProjectModel
+from .models import Post as PostModel, Project as ProjectModel, Tag as TagModel
 from .util import markdown
 
 
@@ -15,64 +15,121 @@ class DateTime(graphene.ObjectType):
     '''
     A full timestamp.
     '''
-    tzinfo = graphene.StringField()
-    year = graphene.IntField()
-    month = graphene.IntField()
-    day = graphene.IntField()
-    hour = graphene.IntField()
-    minute = graphene.IntField()
-    second = graphene.IntField()
-    microsecond = graphene.IntField()
+    tzinfo = graphene.String()
+    year = graphene.Int()
+    month = graphene.Int()
+    day = graphene.Int()
+    hour = graphene.Int()
+    minute = graphene.Int()
+    second = graphene.Int()
+    microsecond = graphene.Int()
+
+    def resolve_tzinfo(self, *args, **kwargs):
+        return self._root.tzinfo
+
+    def resolve_year(self, *args, **kwargs):
+        return self._root.year
+
+    def resolve_month(self, *args, **kwargs):
+        return self._root.month
+
+    def resolve_day(self, *args, **kwargs):
+        return self._root.day
+
+    def resolve_hour(self, *args, **kwargs):
+        return self._root.hour
+
+    def resolve_minute(self, *args, **kwargs):
+        return self._root.minute
+
+    def resolve_second(self, *args, **kwargs):
+        return self._root.second
+
+    def resolve_microsecond(self, *args, **kwargs):
+        return self._root.microsecond
 
 
 class Tag(graphene.ObjectType):
     '''
     A single tag. Sometimes referred to as a keyword.
     '''
-    id = graphene.IDField()
-    url = graphene.StringField()
-    name = graphene.StringField()
-    description = graphene.StringField()
+    id = graphene.ID()
+    url = graphene.String()
+    name = graphene.String()
+    description = graphene.String()
+
+    def resolve_id(self, *args, **kwargs):
+        return self._root.id
+
+    def resolve_name(self, *args, **kwargs):
+        return self._root.name
+
+    def resolve_description(self, *args, **kwargs):
+        return self._root.description
 
     def resolve_url(self, *args, **kwargs):
         # TODO: this URL route should not be hardcoded here
-        return '/tags/' + self.slug
+        return '/tags/' + self._root.slug
 
 
 class Image(graphene.ObjectType):
     '''
     An image.
     '''
-    url = graphene.StringField()
-    width = graphene.IntField()
-    height = graphene.IntField()
+    url = graphene.String()
+    width = graphene.Int()
+    height = graphene.Int()
 
     def resolve_url(self, *args, **kwargs):
         # TODO: this URL route should REALLY not be hardcoded here
-        return 'http://' + settings.HOST_NAME + self.url
+        return 'http://' + settings.HOST_NAME + self._root.url
+
+    def resolve_width(self, *args, **kwargs):
+        return self._root.width
+
+    def resolve_height(self, *args, **kwargs):
+        return self._root.height
 
 
 class Article(graphene.Interface):
     '''
     A single article.  Interface for posts and projects alike.
     '''
-    id = graphene.IDField()
+    id = graphene.ID()
     created = graphene.Field(DateTime)
     modified = graphene.Field(DateTime)
-    url = graphene.StringField()
-    title = graphene.StringField()
-    subtitle = graphene.StringField()
-    tags = graphene.ListField(Tag)
-    content = graphene.StringField()
+    url = graphene.String()
+    title = graphene.String()
+    subtitle = graphene.String()
+    tags = graphene.List(Tag)
+    content = graphene.String()
     bannerImage = graphene.Field(Image)
 
+    def resolve_id(self, *args, **kwargs):
+        return self._root.id
+
+    def resolve_created(self, *args, **kwargs):
+        return self._root.created
+
+    def resolve_modified(self, *args, **kwargs):
+        return self._root.modified
+
+    def resolve_title(self, *args, **kwargs):
+        return self._root.title
+
+    def resolve_subtitle(self, *args, **kwargs):
+        return self._root.subtitle
+
+    def resolve_bannerImage(self, *args, **kwargs):
+        return self._root.bannerImage
+
     def resolve_tags(self, *args, **kwargs):
-        # grab all the tags from the tag manager
-        return self.tags.all()
+        # grab all tags from tag manager
+        return self._root.tags.all()
 
     def resolve_content(self, *args, **kwargs):
-        # render the article's markdown content into HTML
-        return markdown(self.content)
+        # render article's markdown content into HTML
+        return markdown(self._root.content)
 
 
 class Post(Article):
@@ -80,9 +137,10 @@ class Post(Article):
     A single blog post.
     '''
 
+    @graphene.resolve_only_args
     def resolve_url(self, *args, **kwargs):
         # TODO: this URL route should not be hardcoded here
-        return '/posts/' + self.slug
+        return '/posts/' + self._root.slug
 
 
 class Project(Article):
@@ -92,21 +150,25 @@ class Project(Article):
 
     def resolve_url(self, *args, **kwargs):
         # TODO: this URL route should not be hardcoded here
-        return '/projects/' + self.slug
+        return '/projects/' + self._root.slug
 
 
 class Query(graphene.ObjectType):
     '''
     Root level query.
     '''
-    posts = graphene.ListField(Post)
-    projects = graphene.ListField(Project)
-    # post = graphene.Field(Post,
-    #     slug=graphene.Argument(graphene.String)
-    # )
-    # project = graphene.Field(Project,
-    #     slug=graphene.Argument(graphene.String)
-    # )
+    posts = graphene.List(Post)
+    projects = graphene.List(Project)
+    tags = graphene.List(Tag)
+    postsById = graphene.List(Post,
+        ids=graphene.NonNull(graphene.List(graphene.String()))
+    )
+    projectsById = graphene.List(Project,
+        ids=graphene.NonNull(graphene.List(graphene.String()))
+    )
+    tagsById = graphene.List(Tag,
+        ids=graphene.NonNull(graphene.List(graphene.String()))
+    )
 
     @graphene.resolve_only_args
     def resolve_posts(self, *args, **kwargs):
@@ -116,13 +178,21 @@ class Query(graphene.ObjectType):
     def resolve_projects(self, *args, **kwargs):
         return ProjectModel.objects.all()
 
-    # @graphene.resolve_only_args
-    # def resolve_post(self, *args, **kwargs):
-    #     return PostModel.objects.get(slug=kwargs.get('slug'))
-    #
-    # @graphene.resolve_only_args
-    # def resolve_project(self, *args, **kwargs):
-    #     return ProjectModel.objects.get(slug=kwargs.get('slug'))
+    @graphene.resolve_only_args
+    def resolve_tags(self, *args, **kwargs):
+        return TagModel.objects.all()
+
+    @graphene.resolve_only_args
+    def resolve_postsById(self, *args, **kwargs):
+        return PostModel.objects.filter(id__in=kwargs.get('ids'))
+
+    @graphene.resolve_only_args
+    def resolve_projectsById(self, *args, **kwargs):
+        return ProjectModel.objects.filter(id__in=kwargs.get('ids'))
+
+    @graphene.resolve_only_args
+    def resolve_tagsById(self, *args, **kwargs):
+        return TagModel.objects.filter(id__in=kwargs.get('ids'))
 
 
 schema = graphene.Schema(query=Query)
