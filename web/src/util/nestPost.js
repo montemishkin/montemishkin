43bfbc9fc01025f8cd6b81f044d4dab2ce64cd0a
Monte Mishkin <monte@mishkin.com>
2016-01-29 23:42:28 -0800

    `nestPost` utility now handles edge cases of post and tags being undefined

--- web/src/util/nestPost.js
+++ web/src/util/nestPost.js
@@ -1,9 +1,13 @@
-export default (post, tags) => ({
-    ...post,
-    // if post exists
-    tags: typeof post !== 'undefined' && !post.doesNotExist
-        // map tag ids to actual tag objects
-        ? post.tags.map(id => tags[id])
-        // otherwise undefined
-        : void 0,
-})
+export default (post, tags) => typeof post === 'undefined'
+    // return `undefined` if `post` is `undefined`
+    ? void 0
+    // post is not `undefined`
+    : {
+        ...post,
+        // if post exists and has list of tag ids
+        tags: !post.doesNotExist && Array.isArray(post.tags)
+            // map tag ids to actual tag objects
+            ? post.tags.map(id => tags[id])
+            // otherwise undefined
+            : void 0,
+    }
