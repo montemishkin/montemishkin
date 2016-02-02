f157b2fb7c2c8e189b648d6ca9461d705f1efef8
Monte Mishkin <monte@mishkin.com>
2016-02-02 10:33:12 -0800

    now using async google analytics code and only tracking in production

--- web/src/client.js
+++ web/src/client.js
@@ -11,14 +11,21 @@ import routes from 'routes'
 import {createStore} from 'store'
 
 
-// Google Analytics
-/* eslint-disable */
-;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-68929870-2', 'auto');
-/* eslint-enable */
+if (process.env.NODE_ENV === 'production') {
+    // Google Analytics
+    // see: https://developers.google.com/analytics/devguides/collection/analyticsjs/
+    /* eslint-disable */
+    window.ga = window.ga || function () {
+        (ga.q = ga.q || []).push(arguments)
+    };
+    ga.l = +new Date;
+    ga('create', 'UA-68929870-2', 'auto');
+    /* eslint-enable */
+} else {
+    /* eslint-disable */
+    window.ga = window.ga || function () {};
+    /* eslint-enable */
+}
 
 
 // grab initial application state passed from server
