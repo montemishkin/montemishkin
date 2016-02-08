2e8c5ba5905e2f6bcd7858257c221bd955532c4a
Monte Mishkin <monte@mishkin.com>
2016-02-08 02:38:28 -0800

    clean up tests for Spinner component

--- web/src/components/_tests/test_Spinner.js
+++ web/src/components/_tests/test_Spinner.js
@@ -7,7 +7,7 @@ import Spinner from 'components/Spinner'
 
 
 describe('Spinner', function () {
-    it('renders an img element', function () {
+    it('renders an <img>', function () {
         const $spinner = $(<Spinner />).render()
 
         const actual = $spinner.find('img').length
@@ -17,7 +17,7 @@ describe('Spinner', function () {
     })
 
 
-    it('gives the img element the ".spinner" class', function () {
+    it('gives the <img> the ".spinner" class', function () {
         const $spinner = $(<Spinner />).render()
 
         const actual = $spinner.find('img')[0].className
@@ -27,41 +27,41 @@ describe('Spinner', function () {
     })
 
 
-    it('gives the img element a src attribute', function () {
+    it('gives the <img> a src attribute', function () {
         const $spinner = $(<Spinner />).render()
 
         assert.ok($spinner.find('img')[0].src)
     })
 
 
-    it('spreads style prop onto img', function () {
+    it('spreads style prop onto the <img>', function () {
         const style = {backgroundColor: 'red'}
         const $spinner = $(<Spinner style={style} />).render()
 
         const actual = $spinner.find('img')[0].style.backgroundColor
-        const expected = 'red'
+        const expected = style.backgroundColor
 
-        assert.deepEqual(actual, expected)
+        assert.equal(actual, expected)
     })
 
 
-    it('does not let props overwrite src onto img', function () {
+    it('does not let props overwrite src onto the <img>', function () {
         const props = {src: 'this-is-definitely not the img src attribute'}
         const $spinner = $(<Spinner {...props} />).render()
 
         const actual = $spinner.find('img')[0].src
-        const notExpected = 'this-is-definitely not the img src attribute'
+        const notExpected = props.src
 
         assert.notEqual(actual, notExpected)
     })
 
 
-    it('does not let props overwrite className onto img', function () {
+    it('does not let props overwrite className onto the <img>', function () {
         const props = {className: 'this-is-definitely not the img class'}
         const $spinner = $(<Spinner {...props} />).render()
 
         const actual = $spinner.find('img')[0].className
-        const notExpected = 'this-is-definitely not the img class'
+        const notExpected = props.className
 
         assert.notEqual(actual, notExpected)
     })
