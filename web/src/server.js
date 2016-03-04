montemishkin/montemishkin

commit 18c625d6b1125ccff748d7a35c6423edc557d033

now properly using helmet to render <title> on server

web/src/server.js
@@ -71,8 +71,6 @@ server.all('*', async function (req, res) {
             // create redux store
             const store = createStore()
 
-            // initial application state
-            const initialState = JSON.stringify(store.getState())
             // rendered app
             const renderedComponent = renderToString(
                 <App
@@ -85,12 +83,13 @@ server.all('*', async function (req, res) {
             )
 
             // see: https://github.com/nfl/react-helmet#server-usage
-            Helmet.rewind()
+            const helmet = Helmet.rewind()
 
             // render jade template with component mounted
             res.render('index.jade', {
-                initialState,
+                initialState: JSON.stringify(store.getState()),
                 renderedComponent,
+                title: helmet.title,
             })
         }
     })

web/src/templates/index.jade
@@ -8,6 +8,7 @@ html
         script(async, src="//www.google-analytics.com/analytics.js")
         script.
             window.__INITIAL_STATE__ = !{initialState};
+        !{title}
     body
         #app !{renderedComponent}
         script(async, src="/static/client.js", type="text/javascript")

commit 2646ec6d0e2a378e19d21bb0404ab76b3310c037

fixed server rendering bug introduced in commit 18c625d

web/src/server.js
@@ -73,6 +73,13 @@ server.all('*', (req, res) => {
             // create redux store
             const store = createStore()
 
+            // TODO: this should really be grabbed AFTER rendering markup
+            // (so that rendering might alter store).  However, with current
+            // (poor) implementation this will set `isLoading` to true on
+            // things like `state.posts` so the client will be waiting on
+            // a response to a request it didnt make (the server made it)
+            const initialState = JSON.stringify(store.getState())
+
             // rendered app
             const renderedComponent = renderToString(
                 <App
@@ -89,7 +96,7 @@ server.all('*', (req, res) => {
 
             // render jade template with component mounted
             res.render('index.jade', {
-                initialState: JSON.stringify(store.getState()),
+                initialState,
                 renderedComponent,
                 title: helmet.title,
             })