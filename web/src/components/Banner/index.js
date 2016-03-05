70624dd0a9821da64289165f3e99c0d837429f6a
Monte Mishkin <monte@mishkin.com>
2016-03-04 19:36:25 -0800

    sleeker banner

--- web/src/components/Banner/index.js
+++ web/src/components/Banner/index.js
@@ -1,40 +1,40 @@
 // third party imports
 import React, {PropTypes} from 'react'
 import radium from 'radium'
+import {connect} from 'react-redux'
 // local imports
 import styles from './styles'
 
 
 function Banner({
+    isLessThanInfinity,
     Icon,
     title,
     subtitle,
-    children,
     style,
     ...unusedProps,
 }) {
+    const iconStyles = isLessThanInfinity
+        ? styles.iconMedium
+        : styles.iconInfinity
+
     return (
         <header
             {...unusedProps}
             style={[styles.outerContainer, style]}
         >
             <div style={styles.innerContainer}>
-                {Icon && (
-                    <Icon style={styles.icon} />
-                )}
-                <h1 style={styles.title}>
-                    {title}
-                </h1>
-                {subtitle && (
-                    <span style={styles.subtitle}>
-                        {subtitle}
-                    </span>
-                )}
-                {children && (
-                    <div style={styles.childrenContainer}>
-                        {children}
-                    </div>
-                )}
+                {Icon && <Icon style={iconStyles} />}
+                <div style={styles.text}>
+                    <h1 style={styles.title}>
+                        {title}
+                    </h1>
+                    {subtitle && (
+                        <h2 style={styles.subtitle}>
+                            {subtitle}
+                        </h2>
+                    )}
+                </div>
             </div>
         </header>
     )
@@ -49,4 +49,9 @@ Banner.propTypes = {
 }
 
 
-export default radium(Banner)
+function mapStateToProps(state) {
+    return {isLessThanInfinity: state.browser.lessThan.infinity}
+}
+
+
+export default connect(mapStateToProps)(radium(Banner))
--- web/src/components/Banner/styles.js
+++ web/src/components/Banner/styles.js
@@ -8,6 +8,13 @@ import {
 
 
 const spacing = 40
+const iconSideLength = 150
+const iconBase = {
+    transitionDuration: '0.8s',
+    transitionTimingFunction: 'ease-in-out',
+    transitionProperty: 'width margin',
+    height: iconSideLength,
+}
 
 
 export default {
@@ -16,46 +23,48 @@ export default {
         justifyContent: 'center',
         padding: `${spacing}px 0`,
         backgroundColor: colors.secondary.main,
-        borderWidth: '0 0 1px 0',
-        borderStyle: 'solid',
-        borderColor: colors.background.inverse,
+        // TODO: dont hardcode the color here?
+        boxShadow: '0 3px 30px rgba(0, 0, 0, 0.3)',
     },
+
 
     innerContainer: {
         display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '90%',
-        maxWidth: contentMaxWidth + 20,
+        // TODO: this 80% should be a numeric to keep in sync with main content width
+        width: '80%',
+        maxWidth: contentMaxWidth,
     },
 
-    icon: {
-        height: 150,
-        fontSize: '150px',
-        borderStyle: 'solid',
-        borderWidth: 0,
-        borderRadius: 7,
+
+    iconMedium: {
+        ...iconBase,
+        width: 0,
+        margin: 0,
     },
+
+
+    iconInfinity: {
+        ...iconBase,
+        width: iconSideLength,
+        marginRight: spacing,
+    },
+
+
+    text: {
+    },
+
 
     title: {
         fontSize: largestFontSize,
         color: colors.background.inverse,
-        textAlign: 'center',
-        margin: `${spacing}px 0 0 0`,
+        margin: spacing / 2,
     },
+
 
     subtitle: {
         fontSize: largeFontSize,
         color: colors.background.inverse,
+        margin: spacing / 2,
         fontWeight: 'normal',
-        textAlign: 'center',
-        margin: `${spacing / 2}px 0 ${spacing}px 0`,
-    },
-
-    childrenContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '100%',
     },
 }
